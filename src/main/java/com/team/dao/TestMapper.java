3cede54007ff3d32056fc16c220ba7acf81c273b
刘衍 <77131638+coding-LiuYan@users.noreply.github.com>
2021-12-30 17:10:55 +0800

    Delete src directory

--- src/main/java/com/team/dao/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.team.dao;
-
-import com.team.pojo.Test;
-
-import java.util.List;
-
-public interface TestMapper {
-  List<Test> printAll();
-}
--- src/main/java/com/team/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.team.service;
-
-import com.team.pojo.Test;
-
-import java.util.List;
-
-public interface TestService {
-  List<Test> printAll();
-}
