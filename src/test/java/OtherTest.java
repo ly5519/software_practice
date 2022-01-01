9e86e8265405c6479e7c601e801792bf66d29e38
刘衍 <77131638+coding-LiuYan@users.noreply.github.com>
2022-01-01 20:08:23 +0800

    Liu (#33)
    * 补全增删改查： 课程类
    
    * 补全增删改查： Major
    
    * 补全增删改查： student
    
    * 补全增删改查： swc
    
    * 补全增删改查： 管理员
    
    * 删除了部分无用文件（登陆失败）
    
    * admin重写

---
+++ src/test/java/OtherTest.java
@@ -0,0 +1,24 @@
+/**
+ * Data 2022-01-01 17:34
+ *
+ * @author Liu_Yan
+ */
+public class OtherTest {
+
+  public static void main(String[] args) {
+
+    try {
+      String str = "123456";
+      float number = Integer.parseInt(str);
+      System.out.println(number);
+    }catch (Exception e ) {
+      System.out.println(" error: " + e);
+    }
+
+
+
+
+
+
+  }
+}
